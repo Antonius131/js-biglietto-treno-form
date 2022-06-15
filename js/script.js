fbe771daf8bfafa7e907cfc1b095bc48c23e4593
Antonius131 <antomendetta@gmail.com>
2022-06-15 17:42:01 +0200

    Milestone 2: print price calculation in the page

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
+const tripDistance = document.getElementById('distance-input');
+const userAge = document.getElementById('age-input');
 const resultButton = document.getElementById('submit-result');
 
 resultButton.addEventListener('click', function(){
-   console.log(document.getElementById('distance-input').value);
-   console.log(document.getElementById('age-input').value);
+   let ticketPrice = tripDistance.value * .26;
+   parseInt(tripDistance.value);
+   parseInt(userAge.value);
+
+   if (userAge.value < 18) {
+      ticketPrice = ticketPrice * .85;
+   } else if (userAge.value >= 65) {
+      ticketPrice = ticketPrice * .65;
+   }
+   
+   const roundedPrice = ticketPrice.toFixed(2);
+
+   document.getElementById('trip-distance').innerHTML = tripDistance.value;  
+   document.getElementById('user-age').innerHTML = userAge.value;
+   document.getElementById('ticket-price').innerHTML = roundedPrice;
 });
-
-// let ticketPrice = tripDistance * .26;
-
-// if (userAge < 18) {
-//    ticketPrice = ticketPrice * .85;
-// } else if (userAge >= 65) {
-//    ticketPrice = ticketPrice * .65;
-// }
-
-// const roundedPrice = ticketPrice.toFixed(2);
-// console.log(roundedPrice);
