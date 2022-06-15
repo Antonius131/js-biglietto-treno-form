1e0557241a12c98d8a48437ca4daf476aefb83a0
Antonius131 <antomendetta@gmail.com>
2022-06-15 15:15:27 +0200

    Milestone 1: ticketPrice calculation

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@ const tripDistance = parseInt(prompt('Quanti km vuoi percorrere?'));
 console.log(tripDistance);
 const userAge = parseInt(prompt('Inserisci la tua età'));
 console.log(userAge);
+let ticketPrice = tripDistance * .26;
+
+if (userAge < 18) {
+   ticketPrice = ticketPrice * .85;
+} else if (userAge >= 65) {
+   ticketPrice = ticketPrice * .65;
+}
+
+const roundedPrice = ticketPrice.toFixed(2);
+console.log(roundedPrice);
