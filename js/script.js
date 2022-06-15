c11e53931aef3a1053a0aae1be15de6695bbdeb1
Antonius131 <antomendetta@gmail.com>
2022-06-15 18:20:31 +0200

    totale price: #show-result behaviour

--- js/script.js
+++ js/script.js
@@ -4,18 +4,23 @@ const resultButton = document.getElementById('submit-result');
 
 resultButton.addEventListener('click', function(){
    let ticketPrice = tripDistance.value * .26;
+   let message;
    parseInt(tripDistance.value);
    parseInt(userAge.value);
 
    if (userAge.value < 18) {
       ticketPrice = ticketPrice * .85;
+
    } else if (userAge.value >= 65) {
       ticketPrice = ticketPrice * .65;
    }
    
    const roundedPrice = ticketPrice.toFixed(2);
 
-   document.getElementById('trip-distance').innerHTML = tripDistance.value;  
+   document.querySelector('#show-result').classList.remove('d-none');
+   document.querySelector('#show-result').classList.add('d-block');
+
+   document.getElementById('trip-distance').innerHTML = tripDistance.value;
    document.getElementById('user-age').innerHTML = userAge.value;
-   document.getElementById('ticket-price').innerHTML = roundedPrice;
+   document.getElementById('ticket-price').innerHTML = roundedPrice + "€";
 });
