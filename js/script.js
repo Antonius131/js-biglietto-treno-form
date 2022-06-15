7e6a5bcdd880add5917065c7fbb4dbc83bd84b6c
Antonius131 <antomendetta@gmail.com>
2022-06-15 18:50:51 +0200

    ticketPrice calculation fix

--- js/script.js
+++ js/script.js
@@ -4,15 +4,14 @@ const resultButton = document.getElementById('submit-result');
 
 resultButton.addEventListener('click', function(){
    let ticketPrice = tripDistance.value * .26;
-   let message;
    parseInt(tripDistance.value);
    parseInt(userAge.value);
 
    if (userAge.value < 18) {
-      ticketPrice = ticketPrice * .85;
+      ticketPrice *= .85;
 
    } else if (userAge.value >= 65) {
-      ticketPrice = ticketPrice * .65;
+      ticketPrice *= .65;
    }
    
    const roundedPrice = ticketPrice.toFixed(2);
